8e98662996f6f63da2d4c987166d3c51400d17f5
Jaret Varn <jav76@uakron.edu>
2021-12-02 17:56:57 -0500

    Day 2 C#

---
+++ C#/Day2/Program.cs
@@ -0,0 +1,74 @@
+﻿
+
+
+
+string inputPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\git\\AOC-2021\\C#\\Day2\\input.txt";
+string[] fileText = System.IO.File.ReadAllLines(inputPath);
+
+
+static int part1(string[] input)
+{
+    int position = 0;
+    int depth = 0;
+    foreach (string line in input)
+    {
+        string[] split = line.Split(' ');
+        string direction = split[0];
+        int value = int.Parse(split[1]);
+
+        switch (direction)
+        {
+            case "forward":
+                position = position + value;
+                break;
+
+            case "down":
+                depth = depth + value;
+                break;
+
+            case "up":
+                depth = depth - value;
+                break;
+
+            default:
+                break;
+        }
+    }
+    return depth * position;
+}
+
+static int part2(string[] input)
+{
+    int position = 0;
+    int depth = 0;
+    int aim = 0;
+    foreach (string line in input)
+    {
+        string[] split = line.Split(' ');
+        string direction = split[0];
+        int value = int.Parse(split[1]);
+
+        switch (direction)
+        {
+            case "forward":
+                position = position + value;
+                depth = depth + aim * value;
+                break;
+
+            case "down":
+                aim = aim + value;
+                break;
+
+            case "up":
+                aim = aim - value;
+                break;
+
+            default:
+                break;
+        }
+    }
+    return depth * position;
+}
+
+Console.WriteLine(part1(fileText));
+Console.WriteLine(part2(fileText));
